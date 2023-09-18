5416e2c366f33c36b6984dd7d08a3b6f13898626
Déborah Martins <deborahamartins29@gmail.com>
2023-09-18 17:39:54 -0300

    update 18/09

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+produto1 = 20
+produto2 = 10
+
+# Soma
+print(produto1 + produto2)
+# Subtração
+print(produto1 - produto2)
+# Divisão
+print(produto1 / produto2)
+# Divisão inteira
+print(produto1 // produto2)
+# Multiplicação
+print(produto1 * produto2)
+# Módulo (resto da divisão)
+print(produto1 % produto2)
+# Exponenciação
+print(produto1 ** produto2)
+
+x = (10 + 5) * 4
+y = (10 / 2) + (25 * 2 )- (2 ** 2)
+print(x)
+print(y)
---
+++ operadores_comparacao.py
@@ -0,0 +1,13 @@
+saldo = 450
+saque = 200
+
+# Igualdade
+print(saldo == saque)
+# Diferença
+print(saldo != saque)
+# Maior que / maior ou igual
+print(saldo > saque)
+print(saldo >= saque)
+# Menor que / menor ou igual
+print(saldo < saque)
+print(saldo <= saque)
