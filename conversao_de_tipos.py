0c86f98b99309eed4cfffd057c495b2f0d0a5c26
Déborah Martins <deborahamartins29@gmail.com>
2023-09-18 14:28:16 -0300

    update file

---
+++ conversao_de_tipos.py
@@ -0,0 +1,22 @@
+# Numérico para string
+preco = 10.50
+idade = 28
+
+print(str(preco))
+
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+# String para número
+preco = "10.50"
+idade = "28"
+
+print(float(preco))
+
+print(type(int(idade)))
+  
+print(100 / 2)
+# Sem ponto flutuante
+print(100 // 2)
